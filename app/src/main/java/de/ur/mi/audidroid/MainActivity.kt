4b6a2b6aada22281ebd807bfd6ccd848101b4b2d
Sabine Roth <sabi.roth@t-online.de>
2019-12-06 14:49:00 +0100

    Add initial project

---
+++ app/src/main/java/de/ur/mi/audidroid/MainActivity.kt
@@ -0,0 +1,12 @@
+package de.ur.mi.audidroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
