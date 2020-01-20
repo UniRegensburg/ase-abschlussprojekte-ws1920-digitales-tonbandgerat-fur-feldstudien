4d6289e669478924042b6cf6d9b604f811d98817
Sabine Roth <sabi.roth@t-online.de>
2020-01-20 19:00:04 +0100

    Update DatabaseTest.kt

--- app/src/androidTest/java/de/ur/mi/audidroid/models/DatabaseTest.kt
+++ app/src/androidTest/java/de/ur/mi/audidroid/models/DatabaseTest.kt
@@ -1,33 +1,26 @@
 package de.ur.mi.audidroid.models
 
-import org.junit.Test
 import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.core.IsEqual.equalTo
+import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.After
 import java.io.IOException
-import androidx.test.ext.junit.runners.AndroidJUnit4
 
 @RunWith(AndroidJUnit4::class)
 class DatabaseTest {
 
-  /*  @Mock
-    private lateinit var mockContext: Context
-
-    @Mock
-    private lateinit var database: RoomDatabase*/
-
     private lateinit var testEntity: EntryEntity
-
     private lateinit var db: RecorderDatabase
-
     private lateinit var testDao: EntryDao
+    private val testUid = 1
+    private val testUidForInsertTest = 2
 
-    private val testUid = 12
 
     @Before
     fun createDb() {
@@ -37,6 +30,7 @@ class DatabaseTest {
         ).build()
         testDao = db.entryDao()
         testEntity = EntryEntity(testUid, "test", "test", "test")
+        testDao.insert(testEntity)
     }
 
     @After
@@ -47,12 +41,30 @@ class DatabaseTest {
 
     @Test
     @Throws(Exception::class)
-    fun testImport() {
+    fun testLoadEntryById() {
+        val byIdEntity = testDao.loadEntryById(testUid)
+        assertEquals(byIdEntity.uid, testUid)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testInsert() {
+        val testInsertEntity = EntryEntity(testUidForInsertTest, "test", "test", "test")
+        /** checks if the number of entries in the database increments */
         val currentNumberEntries = testDao.getAllRecordings().size
-        testDao.insert(testEntity)
+        testDao.insert(testInsertEntity)
         assertEquals(currentNumberEntries + 1, testDao.getAllRecordings().size)
-        val byId = testDao.loadEntryById(testUid)
-        assertThat(byId, equalTo(testEntity))
+        /** checks if testEntry is in the database */
+        val byId = testDao.loadEntryById(testUidForInsertTest)
+        assertThat(byId, equalTo(testInsertEntity))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testDelete() {
+        testDao.delete(testEntity)
+        val deletedEntity = testDao.loadEntryById(testUid)
+        assertEquals(deletedEntity, null)
     }
 
     @Test
