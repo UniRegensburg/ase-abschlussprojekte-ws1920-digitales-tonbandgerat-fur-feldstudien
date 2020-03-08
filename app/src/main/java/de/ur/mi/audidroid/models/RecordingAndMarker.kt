719e23bcf3fa68bd697fcd7ab00adbc05aa0a402
Theresa Strohmeier <theresa.strohmeier@stud.uni-regensburg.de>
2020-03-08 19:13:32 +0100

    requested changes

--- app/src/main/java/de/ur/mi/audidroid/models/RecordingAndMarker.kt
+++ app/src/main/java/de/ur/mi/audidroid/models/RecordingAndMarker.kt
@@ -3,6 +3,11 @@ package de.ur.mi.audidroid.models
 import androidx.room.Embedded
 import androidx.room.Relation
 
+/**
+ *  RecordingAndMarker maps the one-to-many relationship between a recording and its marks.
+ * @author: Lisa Sanladerer
+ */
+
 data class RecordingAndMarker(
     @Embedded val entryEntity: EntryEntity,
     @Relation(
