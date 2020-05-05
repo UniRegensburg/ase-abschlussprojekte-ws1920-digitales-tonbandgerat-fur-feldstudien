dea5820e39f72f2def7bb0a1461f8b4372a7dd64
Sabine Roth <sabi.roth@t-online.de>
2020-05-05 21:29:36 +0200

    Update FFMpeg

--- app/src/main/java/de/ur/mi/audidroid/utils/FFMPEGCallback.kt
+++ app/src/main/java/de/ur/mi/audidroid/utils/FFMPEGCallback.kt
@@ -2,15 +2,9 @@ package de.ur.mi.audidroid.utils
 
 import java.io.File
 
-/**
- * Callback for file format conversion with FFMPEG, used in [AudioConverter].
- * @author: Jonas Puchinger
- * Adapted from: https://github.com/adrielcafe/AndroidAudioConverter
- */
+interface FFMpegCallback {
 
-interface FFMPEGCallback {
-
-    fun onSuccess(convertedFile: File)
+    fun onSuccess(convertedFile: File, type: String, startTimeInMilli: Int, endTimeInMilli: Int)
 
     fun onFailure(error: Exception)
 }
