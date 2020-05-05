55c0f762656cdb2459abbd37a3534095bff9d67f
JonasPuchinger <j.g.puchinger@kabelmail.net>
2020-05-05 22:56:10 +0200

    Comment SplashActivity

--- app/src/main/java/de/ur/mi/audidroid/views/SplashActivity.kt
+++ app/src/main/java/de/ur/mi/audidroid/views/SplashActivity.kt
@@ -5,6 +5,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import de.ur.mi.audidroid.R
 
+/**
+ * The [SplashActivity] displays the app logo briefly at the start of the app.
+ * Afterwards, the [MainActivity] is launched.
+ * @author: Jonas Puchinger
+ */
+
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
